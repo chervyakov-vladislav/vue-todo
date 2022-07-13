ea4fdb4e2b72368b1914c666e0ce3b97fa7c540f
Vladislav Chervyakov <chervyakovve@mail.ru>
2022-07-13 23:44:13 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template lang="pug">
+div 
+  h2.titile hello world
+  
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+    
+  }
+}
+</script>
+
+<style scoped>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.titile {
+  color: green;
+}
+</style>
